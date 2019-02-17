c025414038f66932a47ec2f7a60a9570bd88aa32
Jentle Yow <lonestaramazed@live.com>
2019-02-17 11:22:11 +0800

    Updates

---
+++ precache-manifest.af7e2b0d919166dcf608528a34450397.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "37c489e366136bd776fe",
+    "url": "/millionaire/static/css/main.96523b3f.chunk.css"
+  },
+  {
+    "revision": "37c489e366136bd776fe",
+    "url": "/millionaire/static/js/main.37c489e3.chunk.js"
+  },
+  {
+    "revision": "6c5eb9570945a4f20a77",
+    "url": "/millionaire/static/js/runtime~main.6c5eb957.js"
+  },
+  {
+    "revision": "aadb54fd07b5785cc17c",
+    "url": "/millionaire/static/js/2.aadb54fd.chunk.js"
+  },
+  {
+    "revision": "e58afcd61d547ebf48f7e6e3386d916b",
+    "url": "/millionaire/static/media/lose.e58afcd6.mp3"
+  },
+  {
+    "revision": "40f2f3274d8e50ea554a85384dc0246b",
+    "url": "/millionaire/static/media/question1.40f2f327.mp3"
+  },
+  {
+    "revision": "66f5ea810567844dc1af25f0bf0562a0",
+    "url": "/millionaire/static/media/logo.66f5ea81.png"
+  },
+  {
+    "revision": "2ba530893133a267e5c65a8dc1d45e2d",
+    "url": "/millionaire/static/media/question2.2ba53089.mp3"
+  },
+  {
+    "revision": "6fad040f3a5ffee809ae85854add1bf2",
+    "url": "/millionaire/static/media/finalanswer.6fad040f.mp3"
+  },
+  {
+    "revision": "ca04dcd5198b1f900a63219a0dd028fc",
+    "url": "/millionaire/static/media/win.ca04dcd5.mp3"
+  },
+  {
+    "revision": "620b8a09168c7126f4859b7f4cac7024",
+    "url": "/millionaire/static/media/box5.620b8a09.png"
+  },
+  {
+    "revision": "fb438a16c4449d8863dbe992d97286c1",
+    "url": "/millionaire/static/media/box1.fb438a16.png"
+  },
+  {
+    "revision": "980a42109b7892986d02ee59d6746118",
+    "url": "/millionaire/static/media/box3.980a4210.png"
+  },
+  {
+    "revision": "f39fcfe43ce1ed77983f7332cb3f3c94",
+    "url": "/millionaire/static/media/box6.f39fcfe4.png"
+  },
+  {
+    "revision": "c58fe1a0b92955b76074c928d668cb0d",
+    "url": "/millionaire/static/media/Purple-Blue-gradient.c58fe1a0.jpg"
+  },
+  {
+    "revision": "e11220af60fd821d729ff90179c5cb92",
+    "url": "/millionaire/static/media/box2.e11220af.png"
+  },
+  {
+    "revision": "27e69a33ba756200cbed6527a2114689",
+    "url": "/millionaire/static/media/box4.27e69a33.png"
+  },
+  {
+    "revision": "55ae880b78e3333f36b944030bb95306",
+    "url": "/millionaire/index.html"
+  }
+];
